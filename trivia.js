a40d4f3490332836067e1cdfdf7c4672ab83d577
JesseChum <jessechum@icloud.com>
2025-09-23 19:34:30 -0700

    Added js file with quetion and answers. added next button and css for it

---
+++ trivia.js
@@ -0,0 +1,11 @@
+const questions = [
+  {
+    question: "Which country is the largest in the world?",
+    answers: [
+      { text: "United States", correct: false },
+      { text: "Russia", correct: true },
+      { text: "China", correct: false },
+      { text: "South America", correct: false },
+    ],
+  },
+];
